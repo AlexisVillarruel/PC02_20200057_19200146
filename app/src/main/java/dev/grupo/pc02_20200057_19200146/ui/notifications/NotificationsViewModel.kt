23202565bb99f6c62773f5876eda5a1c8207cf27
Alexis_Jhair <alexisvillarruelj14@gmail.com>
2024-11-09 11:17:15 -0500

    Initial commit

---
+++ app/src/main/java/dev/grupo/pc02_20200057_19200146/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package dev.grupo.pc02_20200057_19200146.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
